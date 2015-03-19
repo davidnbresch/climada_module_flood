46e1353be8086b06be7a298bbf62fb4a1347a5df
melanie <melanie.bieli@bluewin.ch>
2015-03-19 14:53:09 +0100

    Include the assignment of LAIs to the preparation steps

--- code/fl_centroids_prepare.m
+++ code/fl_centroids_prepare.m
@@ -1,4 +1,4 @@
-function centroids = fl_centroids_prepare(centroids, res, basin_shapefile, check_plots, force_recalc)
+function centroids = fl_centroids_prepare(centroids, res, basin_shapefile, LAI_img_filename, check_plots, force_recalc)
 % Assign flood score and basin IDs to centroids
 % MODULE:
 %   flood
@@ -7,10 +7,12 @@ function centroids = fl_centroids_prepare(centroids, res, basin_shapefile, check
 % PURPOSE:
 %   Equip centroids with flood scores and basin IDs, such that they can be
 %   used for the generation of a flood hazard set. 
-%       In step 1, flood scores are calculated for the given centroids (see
-%       function centroids_fl_score_calc for details)
+%       In step 1, flood scores and wetness indices are calculated for the 
+%       given centroids (see function centroids_fl_score_calc for details)
 %       In step 2, basin IDs are assigned to the centroids (see function
 %       centroids_basinID_assign for details)
+%       In step 3, Leaf Area Indices (LAIs) are calculated for the
+%       centroids (see centroids_LAI_assign for details)
 % CALLING SEQUENCE:
 %   centroids = fl_centroids_prepare(centroids, res, basin_shapefile, check_plots)
 % EXAMPLE:
@@ -27,6 +29,8 @@ function centroids = fl_centroids_prepare(centroids, res, basin_shapefile, check
 %   to the shapefile names as provided by HydroSHEDS. The basin shapefiles
 %   need to be located in the data/system folder of the CLIMADA flood
 %   module. 
+%   LAI_img_filename: The filename of the global annual mean LAI image (if
+%   empty, the default image will be used)
 %   res: resolution of shape file - the HydroSHEDS basin outline files are
 %   provided in 15 arcsecond (res==15) and 30 arcsecond (res==30; default)
 %   resolution.
@@ -36,12 +40,14 @@ function centroids = fl_centroids_prepare(centroids, res, basin_shapefile, check
 %   force_recalc: if set to 1, recalculate flood scores and basin IDs, even
 %   if they already exist (default is 0)
 % OUTPUT:
-%   centroids: centroids with two additional fields: 
+%   centroids: centroids with three additional fields: 
 %       centroids.flood_score: flow accumulation for each centroid
 %       centroids.basin_ID: river basins the centroid have been assigned to
+%       centroids.LAI: Leaf Area Index
 %  
 % MODIFICATION HISTORY:
 % Melanie Bieli, melanie.bieli@bluewin.ch, 20150311, initial
+% Melanie Bieli, melanie.bieli@bluewin.ch, 20150319, added LAI
 
 global climada_global
 
@@ -54,18 +60,24 @@ if ~exist('centroids','var') || isempty(centroids)
     return; 
 end
 if ~exist('basin_shapefile','var') || isempty(basin_shapefile)
-    basin_shapefile=''; end
+    basin_shapefile='';     end
+if ~exist('LAI_img_filename','var') || isempty(LAI_img_filename)
+    LAI_img_filename='';    end
 if ~exist('check_plots', 'var') || isempty(check_plots)
-    check_plots = 0;    end
+    check_plots = 0;        end
 if ~exist('force_recalc','var') || isempty(force_recalc), 
-    force_recalc = 0;   end
+    force_recalc = 0;       end
 
 fprintf('Preparing %i centroids...\n', length(centroids.centroid_ID))
 % Step 1: Calculate flood scores 
-centroids = centroids_fl_score_calc(centroids, check_plots);
+centroids = centroids_fl_score_calc(centroids, check_plots, force_recalc);
 
 % Step 2: Assign basin IDs
 centroids = centroids_basinID_assign(centroids, res, basin_shapefile, ...
+    check_plots, force_recalc);
+
+% Step 3: Assign Leaf Area Indices (LAIs)
+centroids = centroids_LAI_assign(centroids, LAI_img_filename, ...
     check_plots, force_recalc);
 end
 
