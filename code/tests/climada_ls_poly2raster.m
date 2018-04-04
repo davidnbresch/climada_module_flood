db6c5d39c84d6ca77a4b2c9d25498cbd03f5fb04
Thomas <9593280+ThomasRoelli@users.noreply.github.com>
2018-04-04 09:15:47 +0200

    with buffer

--- code/tests/climada_ls_poly2raster.m
+++ code/tests/climada_ls_poly2raster.m
@@ -1,4 +1,4 @@
-function raster = climada_ls_poly2raster(lon,lat,S,field,buffer)
+function merge = climada_ls_poly2raster(lon,lat,S,field,buffer)
 
 % Transform polygons into raster data
 % MODULE:
@@ -8,18 +8,33 @@ function raster = climada_ls_poly2raster(lon,lat,S,field,buffer)
 % PURPOSE:
 %   Transform polygons from a shape-structure into a raster-grid. Function
 %   uses polygon2GRIDobj from TopoToolbox and adds an optional buffer. The
-%   buffer is useful when the resolution of the DEM is sparse (without
-%   buffer: would be inconsistent with too small area.)
+%   TopoToolbox function transforms each gridpoint enclosed by the polygon
+%   (or lying within the polygon) to a rastercell and labels it according
+%   to the name or number of the field of the polygon. When a buffer is set
+%   the area is widened and accordinlgy, the number of cells lying within 
+%   the polygon is increased.
 % CALLING SEQUENCE:
 %   climada_ls_hazard_sets
 % EXAMPLE:
-%   
+%   transformation with a buffer of 1arcsec. no field given --> logical
+%   transformation (0/1).
+%    raster = climada_ls_poly2raster(lon,lat,S,'',1/3600);
+%    tranformation while it takes writes the information from TARGED_FID
+%   for cells within the corresponding polygon. Without buffer
+%    raster = climada_ls_poly2raster(lon,lat,S,'TARGET_FID');
 % INPUTS: 
-%    
+%    lon: (nxm)-matrix. longitutional information about the position of the desired grid
+%    lat: (nxm)-matrix. latitutional information about the position of the desired grid
+%    S:   mapstruct of a polyline as imported by shaperead. Must have
+%         the fields X and Y with the coordinates of the 
 % OPTIONAL INPUT PARAMETERS:
-% 
+%    field: name of the numeric data to be mapped. If not
+%           provided, polygon2GRIDobj will map logical values.
+%    buffer: Scalar which is specified in degrees of
+%            arc and defines the width of the bufferzone along the polygon
+%            (default = 0)
 % OUTPUTS:
-%  
+%    raster: Transformed raster grid with same dimension as lat and lon
 % MODIFICATION HISTORY:
 % Thomas Rölli, thomasroelli@gmail.com, 20180327, init
 
@@ -29,18 +44,28 @@ if ~climada_init_vars, return; end
 % check arguments
 if ~exist('lon', 'var'), lon = []; end
 if ~exist('lat', 'var'), lat = []; end
-if ~exist('polygon', 'var'), polygon = []; end
-if ~exist('field', 'var'), end
+if ~exist('field', 'var'), field = []; end
 if ~exist('buffer', 'var'), buffer = []; end
 
 % PARAMETERS 
 if isempty(lon); return; end
 if isempty(lat); return; end
-if isempty(polygon); return; end
-if isempty(field); end
+if isempty(field); field = []; end
 if isempty(buffer); buffer = 0; end
 
 DEM = GRIDobj(lon,lat,zeros(size(lon)));
-P = polygon2GRIDobj(DEM,S,'TARGET_FID');
+if isempty(field); P = polygon2GRIDobj(DEM,S); else P = polygon2GRIDobj(DEM,S,field); end
+
+%generate buffer around original shape file
+Sb = S;
+for i = 1:numel(S)
+    [latb,lonb] = bufferm(S(i).Y,S(i).X,buffer);
+    Sb(i).X = lonb;
+    Sb(i).Y = latb;
+end
+
+if isempty(field); Pb = polygon2GRIDobj(DEM,Sb); else Pb = polygon2GRIDobj(DEM,Sb,field); end
+
+merge = P.Z+Pb.Z;
 
 end
