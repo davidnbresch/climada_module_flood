2d31547b64ab56fbdeeec082f2bee2c59e38c8b5
Thomas <9593280+ThomasRoelli@users.noreply.github.com>
2018-04-18 17:03:15 +0200

    calculation of area, start, end and length

--- code/tests/climada_ls_FPcalibration.m
+++ code/tests/climada_ls_FPcalibration.m
@@ -36,8 +36,60 @@ lon = reshape(centroids.lon,n_lat,n_lon);
 lat = reshape(centroids.lat,n_lat,n_lon);
 elevation = reshape(centroids.elevation_m,n_lat,n_lon);
 
-polyraster = climada_ls_poly2raster(lon,lat,S,'Ereignisnu');
+polyraster = climada_ls_poly2raster(lon,lat,S,'OBJECTID');
 
-[8.111086 8.341411 46.815369 46.946240]
+%%
+%%calculate area of each polygon in raster%%
+%is saved in polygon structure
+
+%get unitarea
+deg_km = 111.12;
+deg_km = 111.132;
+dlat = abs(min(diff(lat(:,1)))); 
+dlon = abs(min(diff(lon(1,:))));
+dy = dlat*(deg_km * 1000);
+dx = dlon*cosd(mean(lat(:,1)))*(deg_km * 1000); 
+unitarea = dx*dy;
+%dy dx are not exactly right and are different to the one when taking dx
+%and dy in ArcGIS --> ArcGIS determines dx dy more precisely
+%maybe use deg_km = 111.132;
+
+%area for each slide
+for i = 1:numel(S)
+    raster_area = sum(sum(polyraster == S(i).OBJECTID))*unitarea;
+    S(i).Rasterarea = raster_area;
+    %difference Polygon (calculated in arcgis) and raster
+    S(i).Areadiff = S(i).Shape_Area-S(i).Rasterarea;
+end
+
+%%
+%%get starting and end points
+%starting point: maximum of
+start_area = zeros(size(polyraster));
+end_area = zeros(size(polyraster));
+for i = 1:numel(S)
+    %extract slide
+    slide = polyraster == S(i).OBJECTID;
+    
+    %find maximum elevation of slide --> source area
+    [ymax,xmax] = find(elevation.*slide == max(elevation(slide)));
+    start_area(ymax,xmax) = S(i).OBJECTID;
+    
+    %find minimum elvation of slide --> end area
+    [ymin,xmin] = find(elevation.*slide == min(elevation(slide)));
+    end_area(ymin,xmin) = S(i).OBJECTID;
+    i
+end
+
+
+%%
+%%%%%ToDo%%%%
+%*fläche berechnen --> zähle zellen für jeden slide(Ereignisnummer)
+% vergleiche mit ARCGIS
+%*source area finden --> höchster punkt auch mit alti3d2m (gleiche lat lon
+% auflösung--> nicht gleiche x y auflösung)
+%*flow distance berechenen --> distance höchster tiefster punkt (auch mit
+%alti3d_2m)
+%%%%%%%%%%%%%
 
 end
