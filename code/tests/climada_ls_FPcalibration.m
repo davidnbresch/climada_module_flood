28fdb64d8ef292adc13cc78781519e780aff2d3f
Thomas <9593280+ThomasRoelli@users.noreply.github.com>
2018-04-23 13:43:01 +0200

    documentation

--- code/tests/climada_ls_FPcalibration.m
+++ code/tests/climada_ls_FPcalibration.m
@@ -36,6 +36,11 @@ lon = reshape(centroids.lon,n_lat,n_lon);
 lat = reshape(centroids.lat,n_lat,n_lon);
 elevation = reshape(centroids.elevation_m,n_lat,n_lon);
 
+%[slope,~,area,~] = climada_centroids_scores(lon,lat,elevation,0);
+
+DEM = GRIDobj(lon,lat,elevation);
+
+%transform polygon in raster
 polyraster = climada_ls_poly2raster(lon,lat,S,'OBJECTID');
 
 %%
@@ -44,11 +49,13 @@ polyraster = climada_ls_poly2raster(lon,lat,S,'OBJECTID');
 
 %get unitarea
 deg_km = 111.12;
-deg_km = 111.132;
+%deg_km = 111.132;
 dlat = abs(min(diff(lat(:,1)))); 
 dlon = abs(min(diff(lon(1,:))));
 dy = dlat*(deg_km * 1000);
 dx = dlon*cosd(mean(lat(:,1)))*(deg_km * 1000); 
+dx = 2.03326435575496;
+dy = 2.97652555692817;
 unitarea = dx*dy;
 %dy dx are not exactly right and are different to the one when taking dx
 %and dy in ArcGIS --> ArcGIS determines dx dy more precisely
@@ -70,23 +77,31 @@ end_area = zeros(size(polyraster));
 length = zeros(size(polyraster));
 for i = 1:numel(S)
     %extract slide
-    slide = polyraster == S(i).OBJECTID;
+    %slide = polyraster == S(i).OBJECTID;
+    slide = find(polyraster==S(i).OBJECTID);
+    
+    [~,imax] = max(elevation(slide));
+    start_area(slide(imax)) = S(i).OBJECTID;
     
     %find maximum elevation of slide --> source area
-    [ymax,xmax] = find(elevation.*slide == max(elevation(slide)));
-    start_area(ymax,xmax) = S(i).OBJECTID;
+    %[ymax,xmax] = find(elevation.*slide == max(elevation(slide)));
+    %start_area(ymax,xmax) = S(i).OBJECTID;
     
     %find minimum elvation of slide --> end area
-    [ymin,xmin] = find(elevation.*slide == min(elevation(slide)));
-    end_area(ymin,xmin) = S(i).OBJECTID;
+    [~,imin] = min(elevation(slide));
+    end_area(slide(imin)) = S(i).OBJECTID;
+    %[ymin,xmin] = find(elevation.*slide == min(elevation(slide)));
+    %end_area(ymin,xmin) = S(i).OBJECTID;
     
     %get distance between start and end (with dx and dy)
+    [ymax,xmax] = ind2sub(size(start_area),slide(imax));
+    [ymin,xmin] = ind2sub(size(start_area),slide(imin));
+    
     dys = abs(ymax-ymin)*dy;
     dxs = abs(xmax-xmin)*dx;
     dz = max(elevation(slide))-min(elevation(slide));
-    lgt = sqrt(sqrt(dys^2+dxs^2)+dz^2);
+    lgt = sqrt(sqrt(dys^2+dxs^2)^2+dz^2);
     S(i).length = lgt;
-    i
 end
 
 
