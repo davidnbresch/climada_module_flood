5d616ec211f8f2c16d9512997ce83c35badfc656
Thomas <9593280+ThomasRoelli@users.noreply.github.com>
2018-04-30 09:08:40 +0200

    saving procedure

--- code/tests/testing/grid2tiff.m
+++ code/tests/testing/grid2tiff.m
@@ -1,11 +1,11 @@
-function grid2tiff(lon,lat,grid,filename)
+function grid2tiff(lon,lat,grid,file)
 %not a climada function
 %transforms gridded data(n_lon,n_lat) into geotiff, which than can be
 %used in ArcGIS
 %intput:
 %    lon,lat,grid:  grid(n_lon,n_lat) which should be transformed in a
 %                   tiff-file
-%    filename:      string of the name of the tiff file (without '.tif'). File is saved
+%    file:      string of the name of the tiff file (without '.tif'). File is saved
 %                   in '.\climada_data\geotiff'. If not given --> user is
 %                   asked for path and filename.
 
@@ -16,7 +16,7 @@ if ~climada_init_vars, return; end
 if ~exist('lon', 'var'), lon = []; end
 if ~exist('lat', 'var'), lat = []; end
 if ~exist('grid', 'var'), grid = []; end
-if ~exist('filename', 'var'), filename = []; end
+if ~exist('file', 'var'), file = []; end
 
 %check if folder to save geotiff exists
 folder = [climada_global.data_dir filesep 'geotiff'];
@@ -26,23 +26,27 @@ end
 
 %hazard_set_file      = [climada_global.data_dir filesep 'hazards' filesep 'LSXX_hazard.tif'];
 %get path to save
-if isempty(filename) % local GUI
+if isempty(file) % local GUI
     path      = [folder filesep 'grid2tiff.tif'];
-    [filename,folder] = uiputfile(path, 'Save geotiff as:');
-    if isequal(filename,0) || isequal(folder,0)
+    [name,folder] = uiputfile(path, 'Save geotiff as:');
+    if isequal(name,0) || isequal(folder,0)
         return; % cancel
     else
-        savepth = fullfile(folder,filename);
+        savepth = fullfile(folder,name);
     end
 else
-    savepth = [folder filesep filename '.tif'];
+    [filepath,name,ext] = fileparts(file);
+    if isempty(filepath) filepath = folder; end 
+    if isempty(ext) ext = '.tif'; end
+    savepth = [filepath filesep name ext];
 end
 
+[filepath,name,ext] = fileparts(savepth);
 
 DEM = GRIDobj(lon,lat,grid);
 GRIDobj2geotiff(DEM,savepth);
 
-fprintf('%s.tif saved in: \n %s',filename,folder)
+fprintf('%s.tif saved in: \n %s',name,filepath)
 
 
 
