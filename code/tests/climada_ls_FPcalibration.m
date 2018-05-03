a489810fec7a53d03f3cbd828e6858145363a9fc
Thomas <9593280+ThomasRoelli@users.noreply.github.com>
2018-05-03 09:15:59 +0200

    restructured

--- code/tests/climada_ls_FPcalibration.m
+++ code/tests/climada_ls_FPcalibration.m
@@ -1,4 +1,5 @@
-function climada_ls_FPcalibration(calculate,calculate2)
+function S = climada_ls_FPcalibration(orgS,orgLon,orgLat,orgStart,orgEnd,...
+    lon,lat,elevation,field,save_name)
 
 % Script to calibrate flow path parameters
 % MODULE:
@@ -19,7 +20,7 @@ function climada_ls_FPcalibration(calculate,calculate2)
 % OUTPUTS:
 %    
 % MODIFICATION HISTORY:
-% Thomas Rölli, thomasroelli@gmail.com, 20180417, init
+% Thomas Rölli, thomasroelli@gmail.com, 20180502, init
 
 global climada_global
 if ~climada_init_vars, return; end
@@ -27,95 +28,77 @@ if ~climada_init_vars, return; end
 if ~exist('calculate') calculate=0; end
 if ~exist('calculate2') calculate2=0; end
 
+%init S
+S = [];
 
-load('C:\Users\Simon Rölli\Desktop\data\centroids_large\_LS_Sarnen_alti3d_2m.mat','centroids');
-load('C:\Users\Simon Rölli\Desktop\data\centroids_large\_LS_Sarnen_interpol_alti3d.mat','centroids');
+n_lon = size(lon,2);
+n_lat = size(lat,1);
+start = zeros(n_lat,n_lon);
+end_ = zeros(n_lat,n_lon);
 
-%read in DEM (original alti3d with 2x2m resolution is best choice --> best results when assessing source
-%area lenght and width of slides
-n_lon = numel(unique(centroids.lon));
-n_lat = numel(unique(centroids.lat));
-lon = reshape(centroids.lon,n_lat,n_lon);
-lat = reshape(centroids.lat,n_lat,n_lon);
-elevation = reshape(centroids.elevation_m,n_lat,n_lon);
+%caculation of unitarea --> not considering slope
+deg_km = 111.32;
+dlat = abs(min(diff(lat(:,1)))); 
+dlon = abs(min(diff(lon(1,:))));
+dy = dlat*(deg_km * 1000);
+dx = dlon*cosd(mean(lat(:,1)))*(deg_km * 1000); 
+unitarea = dx*dy;
 
-n_lon_10m = numel(unique(centroids.lon));
-n_lat_10m = numel(unique(centroids.lat));
-lon_10m = reshape(centroids.lon,n_lat_10m,n_lon_10m);
-lat_10m = reshape(centroids.lat,n_lat_10m,n_lon_10m);
-elevation_10m = reshape(centroids.elevation_m,n_lat_10m,n_lon_10m);
-clear centroids;
+%prepare grid to use in dsearchn()
+grid = [lon(:) lat(:)];
 
-if calculate
-    %read in shape file of past shallow landslides
-    %S = shaperead('C:\Users\Simon Rölli\Desktop\data\inventory\slides_forMatlab.shp');
-    S = shaperead('C:\Users\Simon Rölli\Desktop\data\inventory\be_polygon_forMatlab.shp');
-    field = 'ID';
+%when get to coarser resolution --> 
+%indices of start areas
+idx_st = find(orgStart~=0); %indices of original start
+points = [orgLon(idx_st) orgLat(idx_st)];
+k_st = dsearchn(grid,points); 
 
-    [S,polyraster,start_area,end_area] = climada_ls_scoresShapefile(lon,lat,elevation,S,field,0);
+idx_en = find(orgEnd~=0); %indices of original end
+points = [orgLon(idx_en) orgLat(idx_en)]; %original points
+k_en = dsearchn(grid,points); %get indices of nearest point to original points
 
-%     figure
-%     plot([S.AREA_GIS],[S.P_AREA_noSL],'*')
-%     figure
-%     plot([S.AREA_GIS],[S.R_AREA_noSL],'*')
-%     figure
-%     plot([S.AREA_GIS],[S.R_AREA_SL],'*')
-%     figure
-%     plot([S.AREA_GIS],[S.R_AREA_SL],'*')
-
-    %calculate ratio of length/shape length
-    C = num2cell([S.R_LGT_noSL]./[S.SHAPE_Leng]);
-    [S.LGT_RAT_noSL] = C{:};
-    %with slope
-    C = num2cell([S.R_LGT_SL]./[S.SHAPE_Leng]);
-    [S.LGT_RAT_SL] = C{:};
-
-    shapewrite(S,'C:\Users\Simon Rölli\Desktop\data\calibration\be_fromMatlab.shp');
-    grid2tiff(lon,lat,start_area,'C:\Users\Simon Rölli\Desktop\data\calibration\start_area.tif');
-    grid2tiff(lon,lat,end_area,'C:\Users\Simon Rölli\Desktop\data\calibration\end_area.tif');
-    grid2tiff(lon,lat,polyraster,'C:\Users\Simon Rölli\Desktop\data\calibration\polyraster.tif');
+maxfield='AREA_GIS';
+for i=1:numel(orgS)
+    S(i).(field) = orgS(i).(field); %transform field ID
+    
+    ik_st = find(orgStart(idx_st) == orgS(i).(field)); %find corresponding index of field ID
+    ik_en = find(orgEnd(idx_en) == orgS(i).(field));
+    %write start and end coordinates in Structure
+    S(i).start = [lon(k_st(ik_st)) lat(k_st(ik_st))];
+    S(i).end = [lon(k_en(ik_en)) lat(k_en(ik_en))];
+    
+    %calculate distance of slide in new raster (with slope)
+    [Ist,Jst] = ind2sub(size(lon),k_st(ik_st));
+    [Ien,Jen] = ind2sub(size(lon),k_en(ik_en));
+    dI = abs(Ist-Ien)*dy;
+    dJ = abs(Jst-Jen)*dx;
+    dz = elevation(k_st(ik_st))-elevation(k_en(ik_en));
+    try 
+        lgt = double(sqrt(sqrt(dI^2+dJ^2)^2+dz^2));
+    catch
+        lgt = double(0);
+    end
+    %write transformed length in structure
+    S(i).t_length = lgt;
+    
 end
-if calculate2
-   S = shaperead('C:\Users\Simon Rölli\Desktop\data\calibration\be_fromMatlab.shp');
-%    grid = GRIDobj('C:\Users\Simon Rölli\Desktop\data\calibration\polyraster.tif');
-%    polyraster = flipud(grid.Z);
-   grid = GRIDobj('C:\Users\Simon Rölli\Desktop\data\calibration\start_area.tif');
-   start_area = flipud(grid.Z);
-   grid = GRIDobj('C:\Users\Simon Rölli\Desktop\data\calibration\end_area.tif');
-   end_area = flipud(grid.Z);
-   clear grid;
-   
-    %load 10m interpolated alti3d data
-    
-    %find nearest cell in 10m grid and assign corresponding value 
-    start_10m = zeros(n_lat_10m,n_lon_10m);
-    end_10m = zeros(n_lat_10m,n_lon_10m);
-    %prepare grid to use in dsearchn()
-    grid_10m = [lon_10m(:) lat_10m(:)];
-    
-    %indices of start areas
-    idx = find(start_area~=0);
-    points = [lon(idx) lat(idx)];
-    k = dsearchn(grid_10m,points);
-    start_10m(k) = start_area(idx);
-    
-    %indices of end areas
-    idx = find(end_area~=0);
-    points = [lon(idx) lat(idx)];
-    k = dsearchn(grid_10m,points);
-    end_10m(k) = end_area(idx);
-    
-    grid2tiff(lon_10m,lat_10m,start_10m,'C:\Users\Simon Rölli\Desktop\data\calibration\start_10m.tif');
-    grid2tiff(lon_10m,lat_10m,end_10m,'C:\Users\Simon Rölli\Desktop\data\calibration\end_10m.tif');
-end
+start(k) = orgStart(idx);
 
-S = shaperead('C:\Users\Simon Rölli\Desktop\data\calibration\be_fromMatlab.shp');
-grid = GRIDobj('C:\Users\Simon Rölli\Desktop\data\calibration\start_10m.tif');
-start_10m = flipud(grid.Z);
-grid = GRIDobj('C:\Users\Simon Rölli\Desktop\data\calibration\end_10m.tif');
-end_10m = flipud(grid.Z);
-clear grid;
 
+%indices of end areas
+idx = find(orgEnd~=0);
+points = [orgLon(idx) orgLat(idx)];
+k = dsearchn(grid,points);
+end_(k) = orgEnd(idx);
+
+
+
+
+
+%area of each raster cell --> considering slope
+cell_area = climada_centroids_area(lon,lat,elevation,0);
+
+%parameters for flowpath
 dH = 0;
 exponent = 25;
 v_max = 4;
@@ -123,17 +106,52 @@ phi = 22;
 delta_i = 0.0003;
 perWt = [1 0.8 0.4 0 0 0 0.4 0.8];
 
-source = zeros(size(elevation_10m));
-elevation_10m = fillsinks(elevation_10m);
-mult_flow = climada_ls_multipleflow(lon_10m,lat_10m,elevation_10m,exponent,dH,1);
-[~,horDist,verDist] = climada_centroids_gradients(lon_10m,lat_10m,elevation_10m);
+%needed data for flowpath
+source = zeros(size(elevation));
+elevation = fillsinks(elevation);
+mult_flow = climada_ls_multipleflow(lon,lat,elevation,exponent,dH,1);
+[~,horDist,verDist] = climada_centroids_gradients(lon,lat,elevation);
 
-for i = 1:numel(S)
-    source(find(start_10m==S(i).ID)) = 1;
+%init of matrix to save flow paths ID and log(maximum intensity)
+slides_ID = zeros(size(elevation));
+slides_int = zeros(size(elevation));
+
+
+%init structure to save results
+S = [];
+
+%dist = 
+all_source = find(start~=0);
+
+while sum(active_cells) > 0
+    for i=1:n_lat
+        for j=1:n_lon
+            tem_sources = find(start~=0)
+            %[I,J] = ind2sub(
+        end
+    end
+    
+end
+
+for i = 1:numel(orgS)
+    source(find(start==orgS(i).(field))) = 1; %find start of slide i and set as source
     spreaded = climada_ls_propagation(source,mult_flow,horDist,verDist,v_max,phi,delta_i,perWt);
+   
+    %calculate area
+    %without consideration of slope --> unitarea
+    slide = find(spreaded~=0);
+    raster_area = numel(slide)*unitarea;
+    S(i).unitarea = raster_area;
     
+    %with consideration of slope
+    raster_area_slope = sum(cell_area(slide));
+    S(i).slopearea = raster_area_slope;
     
+    %add to 
+    source(slide) = S(i).(field);
+    forced_slides = forced_slides+source;
     source = source*0;
+    i
 end
 
 
